4a80ee523cbe540978c0eee7c751d70fd88081a4
tyrohellion <51808054+tyrohellion@users.noreply.github.com>
2026-08-07 13:50:35 -0400

    under construction pages

--- src/routes/players/+page.svelte
+++ src/routes/players/+page.svelte
@@ -1,1 +1,22 @@
-<h1>hello</h1>
+<script>
+	import patrick from '$lib/assets/patrick.png';
+</script>
+
+<div class="center">
+	<img src={patrick} alt="patrick" />
+	<h4>Under Construction</h4>
+</div>
+
+<style>
+	.center {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	img {
+		width: 200px;
+		height: auto;
+	}
+</style>
